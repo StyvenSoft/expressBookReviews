745f3095a3ba7f2aa330e80cc95af52c6894a9d7
Styvensoft <steveen.echeverri@gmail.com>
2025-05-24 18:54:25 -0500

    task 4: Get book details based on title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,9 +51,22 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  let filteredBooks = [];
+
+  // Convert array and filter by title
+  Object.keys(books).forEach(key => {
+    if (books[key].title.toLowerCase() === title.toLowerCase()) {
+      filteredBooks.push({ isbn: key, ...books[key] });
+    }
+  });
+
+  if (filteredBooks.length > 0) {
+    res.status(200).json(filteredBooks);
+  } else {
+    res.status(404).json({ message: "No book found with the given title." });
+  }
 });
 
 //  Get book review
